86a832333100609bd060914d1d9a634b6566a312
Johannes Hildingsson <jhildingss@gmail.com>
2016-11-27 21:16:20 +0100

    first translation commit

--- app/scripts/translations/sv.js
+++ app/scripts/translations/sv.js
@@ -22,15 +22,15 @@ sv.data = {
 
   /* Hardware wallets */
   x_Ledger:             'Ledger Nano S',
-  ADD_Ledger_1:         'Connect your Ledger Nano S',
-  ADD_Ledger_2:         'Open the Ethereum application (or a contract application)',
+  ADD_Ledger_1:         'Anslut din Ledger Nano S',
+  ADD_Ledger_2:         'Öppna Ethereumapplikationen (eller en kontraktsapplikation),
   ADD_Ledger_3:         'Verify that Browser Support is enabled in Settings',
   ADD_Ledger_4:         'If no Browser Support is found in settings, verify that you have [Firmware >1.2](https://www.ledgerwallet.com/apps/manager)',
-  ADD_Ledger_0a:        'Re-open MyEtherWallet on a secure (SSL) connection',
+  ADD_Ledger_0a:        'Öppna MyEtherWallet via en säker(SSL) anslutningR',
   ADD_Ledger_0b:        'Re-open MyEtherWallet using [Chrome](https://www.google.com/chrome/browser/desktop/) or [Opera](https://www.opera.com/)',
-  ADD_Ledger_scan:      'Connect to Ledger Nano S',
+  ADD_Ledger_scan:      'Anslut till Ledger Nano S',
 
-  NAV_DeployContract:   'Deploy Contract',
+  NAV_DeployContract:   'Placera ut kontrakt',
 
   /* Navigation*/
   NAV_YourWallets:      'Dina Plånböcker',
